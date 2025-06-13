59ea336e2612041cb42cb193fe216ce284f9e0fc
liwong88 <liwong@microsoft.com>
2025-06-12 19:55:03 -0700

    Update release_download_url

--- libraries/agent.rb
+++ libraries/agent.rb
@@ -7,7 +7,7 @@ module AzurePipelines
     class << self
       def release_download_url(version = nil)
         version ||= agent_attrs['version']
-        ::CGI.escapeHTML("https://vstsagentpackage.azureedge.net/agent/#{version}/vsts-agent-osx-x64-#{version}.tar.gz")
+        ::CGI.escapeHTML("https://download.agent.dev.azure.com/agent/#{version}/vsts-agent-osx-x64-#{version}.tar.gz")
       end
 
       def agent_home
